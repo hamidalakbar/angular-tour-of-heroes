cbf1cc791e9fc00d106d3ed814c2a78687ec4f71
Hamid Alakbarov <hamidal@uw.edu>
2024-01-10 16:00:29 -0800

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { NgFor, NgIf } from '@angular/common';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -24,7 +25,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     AppRoutingModule,
     FormsModule,
     NgFor,
-    NgIf
+    NgIf,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
